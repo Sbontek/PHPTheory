32d2bf635847e71d5cdd17905a4155ecd0d94a30
SusieBonte <bontekoe.s@gmail.com>
2017-06-13 14:09:49 +0200

    Update2

--- week1/assignments.php
+++ week1/assignments.php
@@ -1,6 +1,7 @@
 <html>
 <?php
 //standard tag 
+echo '<pre>';
 ?>
 <?php echo 'this is a standard tag' ?>
 <?php
@@ -32,12 +33,64 @@
  $sampleFloat = 5.6;
  $sampleString = 'This is a sample string';
  
- echo 'this is a int scalar datatype'.gettype($sampleInt);
- echo 'this is a boolean scalar datatype'.gettype($sampleBoolean);
- echo 'this is a float scalar datatype'.gettype($sampleFloat);
- echo 'this is a string scalar datatype'.gettype($sampleString);
+ echo 'this is a int scalar datatype'.gettype($sampleInt), PHP_EOL;
+ echo 'this is a boolean scalar datatype'.gettype($sampleBoolean), PHP_EOL;
+ echo 'this is a float scalar datatype'.gettype($sampleFloat), PHP_EOL;
+ echo 'this is a string scalar datatype'.gettype($sampleString), PHP_EOL;
  
+ // type casting each data type integer float boolean string array object resource null value
 
+ // type casting from array to object
+ 
+ // variable variables
+	$sampleThing = 'Felix';
+	$$sampleThing = 'The Cat';
+	echo 'Felix '.$Felix ;
+	
+ // CONSTANTS in php script
+	define('Happy', ' Joy Joy');
+	echo 'Happy, Happy'.Happy ;
+ // var_dump print_r and debug_zval_dump
+	$sampleArray = array(1, 2, array("cat", "dog", "mendoza"));
+	
+	var_dump($sampleArray);
+	
+	print_r($sampleArray);
+	
+	debug_zval_dump($sampleArray);
+ // isset and empty functions
+ 
+ // assignment operators
+ 
+ // arithmetic operators
+ 
+ // string operators
+ 
+ // comparison operators( 4 equivalence and 2 inequality)
+ 
+ // logical operators
+ 
+ // bitwise operators
+ 
+ // error control operators
+ 
+ // execution operators
+ 
+ // incrementing/decrementing operators
+ 
+ // type operators
+ 
+ // if else statements and nested if else statements
+ 
+ // ternary operator show comparable if else in comment
+ 
+ // experiment with if else combined with isset and empty functions
+ 
+ // switch case statement including default
+ 
+ // while /dowhile for /foreach include break and continue
+ 
+ echo '</pre>';
 ?>
 
 </html>
--- week1/todo.php
+++ week1/todo.php
@@ -7,28 +7,28 @@
 DONE Pas de Standard Tag en Echo Tag toe
 DONE Experimenteer met de deprecated Short Tag, Script Tag en ASP Tags
 DONE Pas de 2 varianten van Single line comments, en de Multi-line comment toe
-Experimenteer met de 4 scalar data-types en toon aan met de gettype-functie welke data-type je gebruikt hebt
-Experimenteer met type-casting voor elke data-type
-Experimenteer met type-casting van array naar een object
-Experimenteer met variable variablen
-Pas CONSTANTS toe in een PHP-script
-Experimenteer met var_dump, print_ren debug_zval_dump
-Pas de isset() en empty() functies toe
-Experimenteer met de veelgebruikte operators: Assignment Operators, de 6 
-Arithmetic Operators, String Operators, Comparison Operators (4 Equivalence 
-Operators en 2 Inequality Operators) en Logical Operators
-Experimenteer met Bitwise Operators, Error Control Operators, Execution Operators, 
-Incrementing/Decrementing Operators en Type Operators
-Experimenteer met if-elseif-else statements, en nested if-else statements
-Experimenteer met de ternaryoperator en laat in een comment een vergelijkbare if/else statement zien
-Experimenteer met een if/else-statement i.c.m. de isset() en empty() functies
-Experimenteer met switch-case statement (incl default)
-Experimenteer met while, do/while, for, for each en pas ook breaken continuetoe
-Pas in 2 verschillende bestanden de 2 manieren voor namespaces toe
-Pas in 1 bestand 2 verschillende namespaces toe
-Pas de namespaces toe in een bestand
-Experimenteer met sub-namespaces
-Experimenteer met namespaces: Dynamic Usage en Aliasing
+ Experimenteer met de 4 scalar data-types en toon aan met de gettype-functie welke data-type je gebruikt hebt
+ Experimenteer met type-casting voor elke data-type
+ Experimenteer met type-casting van array naar een object
+DONE Experimenteer met variable variablen
+DONE Pas CONSTANTS toe in een PHP-script
+DONE Experimenteer met var_dump, print_ren debug_zval_dump
+ Pas de isset() en empty() functies toe
+ Experimenteer met de veelgebruikte operators: Assignment Operators, de 6 
+ Arithmetic Operators, String Operators, Comparison Operators (4 Equivalence 
+ Operators en 2 Inequality Operators) en Logical Operators
+ Experimenteer met Bitwise Operators, Error Control Operators, Execution Operators, 
+ Incrementing/Decrementing Operators en Type Operators
+ Experimenteer met if-elseif-else statements, en nested if-else statements
+ Experimenteer met de ternaryoperator en laat in een comment een vergelijkbare if/else statement zien
+ Experimenteer met een if/else-statement i.c.m. de isset() en empty() functies
+ Experimenteer met switch-case statement (incl default)
+ Experimenteer met while, do/while, for, for each en pas ook breaken continuetoe
+ Pas in 2 verschillende bestanden de 2 manieren voor namespaces toe
+ Pas in 1 bestand 2 verschillende namespaces toe
+ Pas de namespaces toe in een bestand
+ Experimenteer met sub-namespaces
+ Experimenteer met namespaces: Dynamic Usage en Aliasing
 
 
 */
