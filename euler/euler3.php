71714f66774f9de35c25dc94ff3dd40659e586d2
Sbonte <bontekoe.s@gmail.com>
2017-08-29 15:45:53 +0200

    Update 15

---
+++ euler/euler3.php
@@ -0,0 +1,47 @@
+<?php
+
+//  start number 600851475143
+
+$startingNumber = 600851475143;
+
+// pt1 part to generate prime numbers  x % x == 0
+
+
+// pt2 part that sees if the number is divisible by the prime number  $startNumber / $primeNumber = $result
+
+// pt 3 if yes make factor into variable?  or put in array?
+
+// pt4 is $result less than 1?  then compare all factors to see which is highest and print
+
+// pt5 is $result more than 1?  then start again from pt 1.
+
+
+$primes = array();
+for ($x = 2; $x <= 1000; $x++) 
+{
+    $xIsPrime = TRUE;
+    $sqrtX = sqrt($x);
+    foreach ($primes as $prime) if ($prime > $sqrtX || ((!($x % $prime)) && (!$xIsPrime = FALSE))) break;
+    if ($xIsPrime){($primes[] = $x);}
+}
+
+// var_dump($primes);
+foreach ($primes as $maybeFactor)
+{
+    $remainder = $startingNumber / $maybeFactor;
+    if ($remainder == 0)
+    {
+        break;
+    }
+    
+    elseif (is_int($remainder) == TRUE)
+    {
+        echo "factor equals = ".$maybeFactor."with remainder of = ".$remainder;
+    }
+}
+
+
+
+
+
+?>
